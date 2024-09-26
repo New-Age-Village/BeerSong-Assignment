ee1c97cb7bebe303496f1069284fb223815f5b90
New-Age-Village <serokathabang49@gmail.com>
2024-09-26 12:34:11 +0200

    Add project files.

---
+++ BeerSong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class BeerSong
+    {
+        public BeerSong(int num)
+        {
+            for (int i = 99; num < i; i--)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine(i + "  bottle of beer on the wall, " + i + " bottle of beer. Take one down and pass it around");
+                }
+                else
+                {
+                    Console.WriteLine(i + "  bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Assignment;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BeerSong mySong = new BeerSong(0);
+    }
+}
